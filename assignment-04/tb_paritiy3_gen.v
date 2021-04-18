99c8e805928fc5fbb997e48ca9ab207ac3626a56
Mostafa Bijani <58389446+MostafaBJN@users.noreply.github.com>
2021-04-18 13:35:42 +0000

    Update tb_paritiy3_gen.v

--- assignment-04/tb_paritiy3_gen.v
+++ assignment-04/tb_paritiy3_gen.v
@@ -6,9 +6,9 @@
 --  *******************************************************
 --  All Rights reserved (C) 2019-2020
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9831015
+--  Student Name: Mostafa Bijani
+--  Student Mail: mostafa.bjn.80@gmail.com
 --  *******************************************************
 --  Additional Comments:
 --
@@ -23,33 +23,43 @@
 
 module tb_paritiy3 ();
 
-reg a;
-reg b;
-reg c;
+reg A;
+reg B;
+reg C;
 wire f_parity_gen;
 wire f_parity_gen_sop;
 	
 	paritiy3_gen t_paritiy3_gen (
-		.a(a),
-		.b(b),
-		.c(c),
+		.a(A),
+		.b(B),
+		.c(C),
 		.f(f_parity_gen));
 
 	paritiy3_gen_sop t_paritiy3_gen_sop (
-		.a(a), 
-		.b(b), 
-		.c(c), 
+		.a(A), 
+		.b(B), 
+		.c(C), 
 		.f(f_parity_gen_sop));
 
 
 	initial 
 		begin
-		a <= 1'b0;
-		b <= 1'b0;
-		c <= 1'b0;
-		# 10;
-		// write your code here
-		
-	end
+			A <= 1'b0; B <= 1'b0; C <= 1'b0;
+		#10;
+			A <= 1'b0; B <= 1'b0; C <= 1'b1;
+		#10;
+			A <= 1'b0; B <= 1'b1; C <= 1'b0;
+		#10;
+			A <= 1'b0; B <= 1'b1; C <= 1'b1;
+		#10;
+			A <= 1'b1; B<=  1'b0; C <= 1'b0;
+		#10;
+			A <= 1'b1; B <= 1'b0; C <= 1'b1;
+		#10;
+			A <= 1'b1; B <= 1'b1; C <= 1'b0;
+		#10;
+			A <= 1'b1; B <= 1'b1; C <= 1'b1;
+		#10;
+		end
 
 endmodule
