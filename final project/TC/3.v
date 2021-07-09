26a97eaf2ee4bce399907ac415c8cf3741f1ac5b
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-09 22:39:41 +0430

    Create 3.v

---
+++ final project/TC/3.v
@@ -0,0 +1,11 @@
+module temperatureAbnormalityDetector(
+ factotyBaseTemp,
+ factotyTempCoef,
+ tempSensorValue,
+ temperatureAbnormality);
+input [4:0] factotyBaseTemp;
+input [3:0] factotyTempCoef;
+input [3:0] tempSensorValue;
+output temperatureAbnormality;
+ // write your code here, please.
+endmodule
