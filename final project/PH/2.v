6291bc7b923a6bcff7bcf5f78da89b58477f6647
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-10 00:32:32 +0430

    Update 2.v

--- final project/PH/2.v
+++ final project/PH/2.v
@@ -1,3 +1,23 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: bloodPHAnalyzer
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
 module bloodPHAnalyzer(
  bloodPH,
  abnormalityP,
