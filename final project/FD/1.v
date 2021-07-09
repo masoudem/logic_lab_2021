4fb7f63512533fb40ab133216b5361aaea41fbb3
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-09 22:37:58 +0430

    Create 1.v

---
+++ final project/FD/1.v
@@ -0,0 +1,9 @@
+module fallingDetector(
+ fdSensorValue,
+ fdFactoryValue,
+ fallDetected);
+input [7:0] fdSensorValue;
+input [7:0] fdFactoryValue;
+output fallDetected;
+ // write your code here, please.
+endmodul
