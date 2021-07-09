c283ef12f7c1dde63634c23a7e65bb2b4be950b9
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-09 22:38:52 +0430

    Create 1.v

---
+++ final project/TC/1.v
@@ -0,0 +1,11 @@
+module temperatureCalculator(
+ factotyBaseTemp,
+ factotyTempCoef,
+ tempSensorValue,
+ temperature);
+input [4:0] factotyBaseTemp;
+input [3:0] factotyTempCoef;
+input [3:0] tempSensorValue;
+output [7:0] temperature;
+ // write your code here, please.
+endmodule
