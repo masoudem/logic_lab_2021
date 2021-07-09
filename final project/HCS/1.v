c01697be020654f938aabd9e92ad7d1db53ef2d0
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-09 22:41:20 +0430

    Create 1.v

---
+++ final project/HCS/1.v
@@ -0,0 +1,27 @@
+module HealthcareSystemPhase1(
+ pressureData,
+ bloodPH,
+ bloodType,
+ fdSensorValue,
+ fdFactoryValue,
+ factotyBaseTemp,
+ factotyTempCoef,
+ tempSensorValue
+ presureAbnormality,
+ bloodAbnormality,
+ fallDetected,
+ temperatureAbnormality);
+input [5:0] pressureData;
+input [3:0] bloodPH;
+input [2:0] bloodType;
+input [7:0] fdSensorValue;
+input [7:0] fdFactoryValue;
+input [4:0] factotyBaseTemp;
+input [3:0] factotyTempCoef;
+input [3:0] tempSensorValue;
+output presureAbnormality;
+output bloodAbnormality;
+output fallDetected;
+output temperatureAbnormality;
+ // write your code here, please.
+endmodule
