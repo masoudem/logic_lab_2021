0b4efaf13d557491b3ca3f2c5dd18e46bb4a12d1
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-09 22:36:28 +0430

    Create 3.v

---
+++ final project/PH/3.v
@@ -0,0 +1,9 @@
+module bloodAbnormalityDetector(
+ bloodPH,
+ bloodType,
+ bloodAbnormality);
+input [3:0] bloodPH;
+input [2:0] bloodType;
+output bloodAbnormality;
+ // write your code here, please.
+endmodule
