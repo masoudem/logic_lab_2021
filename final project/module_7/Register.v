f8b2c20a855246c7434e6fdd357bbd3d461fd824
masoudem <53109168+masoudem@users.noreply.github.com>
2021-07-20 14:52:49 +0430

    update
    update

---
+++ final project/module_7/Register.v
@@ -0,0 +1,36 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: Register 
+
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+
+module register (
+
+	input        rst ,
+	input        clk ,
+	input        en ,
+	input  [7:0] din ,
+	output [6:0] qout
+);
+
+	/* write your code here */
+	
+	/* write your code here */
+
+endmodule
