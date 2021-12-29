de4213f56b1d8ae57291bc4849b24abd81b670f9
masoud <masoud.emami98@gmail.com>
2021-12-29 09:08:06 +0330

    module_4

---
+++ final_project_2021/module_4/time_calculate.v
@@ -0,0 +1,29 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2021-2022
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: time_calculate
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module time_calculate(
+ time_out,
+ time_in,
+ time);
+input [7:0] time_out;
+input [7:0] time_in;
+output [7:0] time;
+ // write your code here, please.
+endmodule
