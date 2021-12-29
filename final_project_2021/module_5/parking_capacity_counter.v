5813d316917915895d25d32c04638e833137b930
masoud <masoud.emami98@gmail.com>
2021-12-29 09:13:58 +0330

    module_5

---
+++ final_project_2021/module_5/parking_capacity_counter.v
@@ -0,0 +1,29 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2021-2022
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: parking_capacity_counter
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module parking_capacity_counter(
+ new_capacity,
+ parked,
+ empty);
+input [7:0] new_capacity;
+input [2:0] parked;
+output [2:0] empty;
+ // write your code here, please.
+endmodule
