00f0bb42cce2d4681ab963daf48f411ed3abf547
Masoud Emamian <53109168+masoudem@users.noreply.github.com>
2022-06-03 10:28:00 +0430

    Create bmi.v

---
+++ final project/module_1/bmi.v
@@ -0,0 +1,30 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: pressureAbnormalityDetector 
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module bmi(
+ enable,
+ weight,
+height);
+input enable;
+input [7:0] height;
+input [7:0] weight;
+output [7:0]category;
+ // write your code here, please.
+endmodule
