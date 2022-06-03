f3fbad491d5931ca6a0161243c36991e58125ec3
Masoud Emamian <53109168+masoudem@users.noreply.github.com>
2022-06-03 10:36:19 +0430

    Create comparator.v

---
+++ final project/module_1/comparator.v
@@ -0,0 +1,31 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: parityErrorChecker 
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module comparator(
+ category,
+ overweight,
+ normal,
+ underweight);
+  input [7:0] category;
+  output overweight;
+  output normal;
+  output underweight;
+ // write your code here, please.
+endmodule
