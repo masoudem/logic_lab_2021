510a3bdb0d5cd2b1afb13607307c0da00c2e8142
Masoud Emamian <53109168+masoudem@users.noreply.github.com>
2022-06-03 10:59:48 +0430

    Create bmd.v

---
+++ final project/module_3/bmd.v
@@ -0,0 +1,29 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: bmd 
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module bfpfemale(
+bmdrange,
+normal,
+abnormal);
+  input [1:0] bmdrange;
+  output normal;
+  output abnormal;
+ // write your code here, please.
+endmodule
