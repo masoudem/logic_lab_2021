0e2615c7d499c20611b95e8697ae2342928380d8
Masoud Emamian <53109168+masoudem@users.noreply.github.com>
2022-06-03 10:55:23 +0430

    Create bfp.v

---
+++ final project/module_2/bfp.v
@@ -0,0 +1,31 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2020-2021
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: bloodAbnormalityDetector 
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module bfp(
+w,
+h,
+a,
+bfprange);
+  input [7:0] w;
+  input [7:0] h;
+  input [7:0] a;
+  output [7:0] range;
+ // write your code here, please.
+endmodule
