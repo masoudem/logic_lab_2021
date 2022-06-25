d15fccaaed82f0ee79e6a42b5ba163ee440cffb5
Masoud Emamian <53109168+masoudem@users.noreply.github.com>
2022-06-25 08:35:52 +0430

    Create controller_and_register.v

---
+++ final project/module5/controller_and_register.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2021-2022
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: controller_and_register
+-----------------------------------------------------------*/
+
+module controller_and_register(confirm, request, clock, inputData, dataQ, dataP);
+input confirm, request, clock;
+input [7:0] inputData;
+output [6:0] dataQ, dataP;
+ // write your code here, please.
+endmodule
